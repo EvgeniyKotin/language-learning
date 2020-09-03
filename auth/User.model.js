29f2445bc65b4c444490ba7e79d42db5e9dd6fe4
Evgeniy Kotin <kotin.evgeniy.work@gmail.com>
2020-09-03 12:49:23 +0500

    Added model of user

---
+++ auth/User.model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+const crypto = require('crypto')
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    login: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    hash: {
+        type: String,
+        required: true
+    },
+    salt: {
+        type: String,
+        required: true
+    },
+    dateOfRegistration: {
+        type: Date,
+        default: Date.now
+    }
+ })
+
+ UserSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex')
+    console.log(this.salt)
+
+    this.hash = crypto.pbkdf2(password, this.salt, 1000, 64, 'sha512').toString('hex')
+    console.log(this.hash)
+ }
+
+ UserSchema.methods.checkPassword = function(password) {
+     let hashedPassword = crypto.pbkdf2(password, this.salt, 1000, 64, 'sha512').toString('hex')
+     if(hashedPassword === this.hash) {
+         return true
+     } else {
+         return false
+     }
+ }
+ 
+ module.exports = mongoose.model('User', UserSchema)
